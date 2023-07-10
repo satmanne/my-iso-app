2bde3a0945b460e71b3925988aefd5d4968e6bd5
Sateesh Manne <sateeshmanne@Sateeshs-MacBook-Pro.local>
2023-07-10 15:20:22 +0530

    reduced font size

--- src/YogaSandhya.jsx
+++ src/YogaSandhya.jsx
@@ -25,8 +25,8 @@ const YogaSandhya = ({location,footnote,programName,programdescription,time,back
       >
         <div style={{ fontSize: '50px', color: 'white', fontfamily: 'sans-serif', marginBottom: '-20px' }}>
           <b>{programName}</b></div>
-        <div style={{ fontSize: '38px', color: '#ffc266', fontfamily: 'Fedra Serif A Pro', marginBottom: '60px' }}>{programdescription}</div>
-        <div style={{ fontSize: '38px', color: '#ff9900', marginBottom: '-20px'  }}>{time}</div>
+        <div style={{ fontSize: '35px', color: '#ffc266', fontfamily: 'Fedra Serif A Pro', marginBottom: '60px' }}>{programdescription}</div>
+        <div style={{ fontSize: '35px', color: '#ff9900', marginBottom: '-20px'  }}>{time}</div>
         <div style={{ fontSize: '20px', color: 'white', marginBottom: '10px'  }}>{location}</div>
         <button style={{ marginTop: '20px',height: '60px', width : '200px', fontSize: '16px', backgroundColor: '#ffb84d', borderRadius: '8px' }}>Register</button>
         <div style={{ fontSize: '12px', color: 'white', marginBottom: '150px' }}><i>{footnote}</i></div>
